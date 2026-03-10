b4bd45f4cb4da0169dbfccf0582c057f9f87f83e
tonghanh106 <tonghanh106@gmail.com>
2026-03-10 22:23:34 +0700

    dayy code lan 2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("btn").onclick = function(){
 
-    alert("Chào mừng bạn đến với thế giới lập trình Web!");
+    alert("Chào mừng bạn đến với thế giới lập trình Web 123456!");
 
 }
